2e0b8eaa1ff19fac6173a18515100d7378f46aff
Eugenio <Eugenio@Eugenio-PC>
2017-10-05 19:34:42 +0200

    Commit

---
+++ src/daw/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw;
+
+/**
+ *
+ * @author Eugenio
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String edad;
+
+    public Persona(String nombre, String apellido, String edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }    
+}
